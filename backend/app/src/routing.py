e603af4b146075e81bcaa2a8490c8d6bde66b7cb
Leonard Eyer <leonardeyer@fastmail.de>
2024-05-25 21:04:57 +0200

    wip

--- backend/app/src/routing.py
+++ backend/app/src/routing.py
@@ -1,35 +1,27 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
 from vrpy import VehicleRoutingProblem
+from graph import build_example_graph, plot_knn_graph
 
-def cost(distance): 
+
+def cost(distance):
     car_cost = distance * 0.5
     bike_cost = distance * 0.7
     return [car_cost, bike_cost]
 
-# Create graph
-G = nx.DiGraph()
-for v in [1, 2, 3, 4, 5]:
-    G.add_edge("Source", v, cost=cost(10))
-    G.add_edge(v, "Sink", cost=cost(10))
-G.add_edge(1, 2, cost=cost(10))
-G.add_edge(2, 3, cost=cost(10))
-G.add_edge(3, 4, cost=cost(15))
-G.add_edge(4, 5, cost=cost(10))
 
-for v in G.nodes():
-    if v not in ["Source", "Sink"]:
-        G.nodes[v]["demand"] = 1
+G = build_example_graph()
 
-prob = VehicleRoutingProblem(G, mixed_fleet=True, load_capacity=[3, 2], num_vehicles=[1, 4])#, drop_penalty=10000)
-prob.solve()
+for (u, v) in G.edges:
+    G.edges[u, v]['cost'] = cost(G.edges[u, v]['distance'])
+
+for (u, v) in G.edges:
+    print(f"{u=}, {v=}, {G.edges[u, v]['cost'] = }")
+
+plot_knn_graph(G)
+
+prob = VehicleRoutingProblem(G, mixed_fleet=True, load_capacity=[8, 6], num_vehicles=[1, 4])#, drop_penalty=0.1)
+prob.solve(time_limit=5.0)
 
 print(f"{prob.best_value=}")
 print(f"{prob.best_routes=}")
 print(f"{prob.best_routes_load=}")
 print(f"{prob.best_routes_type=}")
-
-
-nx.draw(G, with_labels=True)
-plt.show()
