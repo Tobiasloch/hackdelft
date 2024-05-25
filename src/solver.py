0228dd2597345ae333030c8357964a5420573e7f
Tobias loch <Tobias.loch@mail.de>
2024-05-25 23:36:14 +0200

    specifying interface

---
+++ src/solver.py
@@ -0,0 +1,6 @@
+from networkx import Graph
+from src.vehicle import Vehicle
+
+
+def solve(graph:Graph, vehicles:list[Vehicle]) -> dict[str, list[tuple[int]]]:
+    pass
--- src/vehicle.py
+++ src/vehicle.py
@@ -2,8 +2,12 @@ class Vehicle:
     name:str
     capacity:int
     speed:int
+    type:str
+    count:int
 
-    def __init__(self, name:str, capacity:int, speed:int):
+    def __init__(self, name:str, capacity:int, speed:int, type:str, count:int):
         self.name = name
         self.capacity = capacity
         self.speed = speed
+        self.type = type
+        self.count = count
