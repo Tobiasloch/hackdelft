e78b1fefe5bd9fbe0c5d1b3dd963bb3e98b199f4
Tobias loch <Tobias.loch@mail.de>
2024-05-26 11:17:41 +0200

    deleting speed from vehicle

--- src/vehicle.py
+++ src/vehicle.py
@@ -1,13 +1,11 @@
 class Vehicle:
     name:str
     capacity:int
-    speed:int
     type:str
     count:int
 
     def __init__(self, name:str, capacity:int, speed:int, type:str, count:int):
         self.name = name
         self.capacity = capacity
-        self.speed = speed
         self.type = type
         self.count = count
